e4336f604dc03d4f2707b10861eafcfb07d12303
Satria Bagas <97071381+yourksatra@users.noreply.github.com>
2024-08-17 07:46:12 +0700

    Update langganan.php

--- application/views/langganan.php
+++ application/views/langganan.php
@@ -11,7 +11,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script type="text/javascript" src="https://app.sandbox.midtrans.com/snap/snap.js"
-        data-client-key="SB-Mid-client-fZzSOY1XL-yd2Hyx"></script>
+        data-client-key="your_client_key"></script>
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 </head>
 
